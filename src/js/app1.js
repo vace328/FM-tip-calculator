bae9c874f142cd05459c260baa2023d6ec4f8233
Ana <zlatkovic.ana@gmail.com>
2022-08-17 14:28:04 +0200

    error styling and err msg displaying

--- src/js/app1.js
+++ src/js/app1.js
@@ -16,6 +16,8 @@ let customRadioErrorContainer = document.getElementById("error-custom");
 let error1 = document.getElementById("error-1");
 
 let tipAmountSelected = null;
+let tipIsSelected = false;
+let radioBtnStatus = [];
 
 // let tipPerPerson = 0;
 // let totalPerPerson = 0;
@@ -54,29 +56,63 @@ function validateFormField(field, error) {
   if (!notEmpty(field.value) || field.value == 0) {
     error.textContent = `Can't be 0`;
     //   changeErrorMsg(error[serial], "error-on", `Can't be 0`);
-    //   field.classList.add("error-field");
+    field.classList.add("has-error");
 	return false;
   } else if (!onlyDigits(field.value)) {
     error.textContent = "Has to be a number";
+    field.classList.add("has-error");
 	return false;
   } else {
     error.textContent = "";
+    field.classList.remove("has-error");
 	return true;
   }
 }
+function validateTipOptions(status, error) {  
+  if (status === false) {
+    error.textContent = "Select an option";
+  } else {
+    // error.textContent = "All coool";
+    error.textContent = "";
+  }
+}
+
 function validateCustomRadioField(field, error) {
   if (!notEmpty(field.value)) {
-    field.setAttribute("placeholder", "0");
+    field.setAttribute("placeholder", "Custom");    
   } else if (!onlyDigits(field.value)) {
     error.textContent = "Has to be a number";
+    field.classList.add("custom-radio-has-error");   
   } else {
     error.textContent = "";
+    field.classList.remove("custom-radio-has-error");
   }
 
   if (notEmpty(field.value) && onlyDigits(field.value)) {
     return true;
   }
 }
+
+function VCRF(status, field, error) {
+  if (status === false) {
+    error.textContent = "Select an option";
+  } else if (!notEmpty(field.value)) {
+    field.setAttribute("placeholder", "Custom");
+  } else if (!onlyDigits(field.value)) {
+    error.textContent = "Has to be a number";
+    field.classList.add("custom-radio-has-error");   
+  } else {
+    // error.textContent = "All coool";
+    error.textContent = "";
+    field.classList.remove("custom-radio-has-error");
+  }
+ 
+
+  if (notEmpty(field.value) && onlyDigits(field.value)) {
+    return true;
+  }
+}
+
 
 function calculateTipPerPerson(totalBill, tip, numberOfPeople) {
   totalBill = stringToNumber(totalBill);
@@ -85,6 +121,7 @@ function calculateTipPerPerson(totalBill, tip, numberOfPeople) {
 
   if (tip === 0) {
     return 0;
+   
   } else {
     return (totalBill * tip) / 100 / numberOfPeople;
   }
@@ -113,8 +150,9 @@ btn.addEventListener("click", (i) => {
   // let bill = stringToNumber(billAmountField.value);
   // let people = stringToNumber(numOfPeopleField.value);
 
-  validateCustomRadioField(customRadio, customRadioErrorContainer);
-
+  // validateCustomRadioField(customRadio, customRadioErrorContainer);
+  // validateTipOptions(tipIsSelected, customRadioErrorContainer);
+  VCRF(tipIsSelected, customRadio, customRadioErrorContainer);
   for (let i = 0; i < allInputFields.length; i++) {
     validateFormField(allInputFields[i], errorContainers[i]);
   }
@@ -123,38 +161,53 @@ btn.addEventListener("click", (i) => {
     tipBtns[5].setAttribute("value", customRadio.value);
     tipBtns[5].checked = true;
   }
+
+  if (bill == 0 || people == 0 || bill == "undefined" || people == "undefined" || tipIsSelected == false) {
+    totalPerPersonField.textContent = "0";
+    tipPerPersonField.textContent = "0";
+  } else {
+    totalPerPersonField.textContent = calculateTotalPerPerson(bill, people, tipAmountSelected);
+    tipPerPersonField.textContent = calculateTipPerPerson(bill, tipAmountSelected, people);
+  }
   
-  totalPerPersonField.textContent = calculateTotalPerPerson(bill, people, tipAmountSelected);
-  tipPerPersonField.textContent = calculateTipPerPerson(bill, tipAmountSelected, people);
+  
 });
 
 // Get radio value
 for (let i = 0; i < tipBtns.length; i++) {
+  console.log( tipBtns[i].checked);
   tipBtns[i].addEventListener("change", function () {
     // (tipAmountSelected) ? console.log(tipAmountSelected.value): null;
     if (this !== tipAmountSelected) {
     //   this.setAttribute("checked", "");
 	console.log(this);
       tipAmountSelected = this.value;
+      tipIsSelected = true;
+      validateTipOptions(tipIsSelected, customRadioErrorContainer);
+      // customRadioErrorContainer.textContent="";
     }
 
     if (tipBtns[i].id !== "tip-other") {
       // tipBtns[i].removeAttribute("checked");
       customRadio.value = null;
       customRadio.setAttribute("placeholder", "Custom");
-      customRadio.style.border = "unset";
+      // customRadio.style.border = "unset";
+      customRadio.classList.remove("custom-radio-accent");
+      ;
     }
    	console.log(tipAmountSelected);
   });
 }
 
-// customRadio.style.border = "unset";
+
 customRadio.addEventListener("input", function (evt) {
   if (validateCustomRadioField(customRadio, customRadioErrorContainer)) {    
     document.getElementById("tip-other").value = customRadio.value;
     document.getElementById("tip-other").checked = true;
     tipAmountSelected = customRadio.value;
-	this.style.borderColor = "hsl(172, 67%, 45%)";
-    // console.log(tipAmountSelected);
-  } 
+    tipIsSelected = true;
+    console.log(tipAmountSelected);
+	  // this.style.borderColor = "hsl(172, 67%, 45%)";
+    customRadio.classList.add("custom-radio-accent");
+  }
 });
